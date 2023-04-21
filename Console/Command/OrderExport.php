aab0e27116b3fb2fedabbee3f62e58c7fde53e6f
Exequiel Lares <exequiellares@gmail.com>
2023-04-20 22:55:30 -0500

    2-1-3 Comando CLI agregar argumentos y opciones

--- Console/Command/OrderExport.php
+++ Console/Command/OrderExport.php
@@ -5,9 +5,14 @@ namespace ExequielLares\OrderExport\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class OrderExport extends Command
 {
+    const ARG_NAME_ORDER_ID = 'order-id';
+    const OPT_NAME_SHIP_DATE = 'ship-date';
+    const OPT_NAME_MERCHANT_NOTES = 'notes';
 
     /**
      * @inheritdoc
@@ -15,7 +20,24 @@ class OrderExport extends Command
     protected function configure()
     {
         $this->setName('order-export:run')
-            ->setDescription('Export order to ERP');
+            ->setDescription('Export order to ERP')
+            ->addArgument(
+                self::ARG_NAME_ORDER_ID,
+                InputArgument::REQUIRED,
+                'Order Id'
+            )
+            ->addOption(
+                self::OPT_NAME_SHIP_DATE,
+                'd',
+                InputOption::VALUE_OPTIONAL,
+                'Shipping date in format YYYY-MM-DD'
+            )
+            ->addOption(
+                self::OPT_NAME_MERCHANT_NOTES,
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Merchant notes'
+            );
     }
 
     /**
@@ -25,6 +47,7 @@ class OrderExport extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $output->writeln('Hello World!');
         return 0;
     }
 }
