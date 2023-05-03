dfb11a7008d5587b546f215cccd6df360eb7c81a
Exequiel Lares <exequiellares@gmail.com>
2023-05-03 00:07:48 -0500

    3-1-1 Configuraciones para conexion con API

--- Model/Config.php
+++ Model/Config.php
@@ -6,14 +6,24 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 
 /**
- *
+ * Class Config
+ * @package ExequielLares\OrderExport\Model
  */
 class Config
 {
+
     /**
      *
      */
     const CONFIG_PATH_ENABLED = 'sales/order_export/enabled';
+    /**
+     *
+     */
+    const CONFIG_PATH_API_URL = 'sales/order_export/api_url';
+    /**
+     *
+     */
+    const CONFIG_PATH_API_TOKEN = 'sales/order_export/api_token';
 
     /**
      * @var ScopeConfigInterface
@@ -37,5 +47,27 @@ class Config
     public function isEnabled(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): bool
     {
         return $this->scopeConfig->isSetFlag(self::CONFIG_PATH_ENABLED, $scopeType, $scopeCode);
+    }
+
+    /**
+     * @param string $scopeType
+     * @param string|null $scopeCode
+     * @return string
+     */
+    public function getApiUrl(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): string
+    {
+        $value = $this->scopeConfig->getValue(self::CONFIG_PATH_API_URL, $scopeType, $scopeCode);
+        return ($value !== null) ? (string) $value : '';
+    }
+
+    /**
+     * @param string $scopeType
+     * @param string|null $scopeCode
+     * @return string
+     */
+    public function getApiToken(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): string
+    {
+        $value = $this->scopeConfig->getValue(self::CONFIG_PATH_API_TOKEN, $scopeType, $scopeCode);
+        return ($value !== null) ? (string) $value : '';
     }
 }
